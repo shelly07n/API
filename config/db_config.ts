b4253a90d42eddbe9821a8962ff30f1426565e5c
Shelly <sheltonfdo23@gmail.com>
2023-09-14 17:14:09 +0530

    datatabase created

--- config/db_config.ts
+++ config/db_config.ts
@@ -1,12 +1,22 @@
 import { SequelizeModuleOptions } from "@nestjs/sequelize";
 
 const config: SequelizeModuleOptions = {
+    // dialect: "mysql",
+    // host: "127.0.0.1",
+    // port: 3306,
+    // username: "root",
+    // password: "",
+    // database: "nest_testing",
+    // autoLoadModels: true,
+    // synchronize: true,
+    // sync: { alter: true }, // Remove this for production
+
     dialect: "mysql",
-    host: "127.0.0.1",
+    host: "3.82.45.219",
     port: 3306,
-    username: "root",
-    password: "",
-    database: "nest_testing",
+    username: "shelton",
+    password: "shelton",
+    database: "shelton",
     autoLoadModels: true,
     synchronize: true,
     sync: { alter: true }, // Remove this for production
